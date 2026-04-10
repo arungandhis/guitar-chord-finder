08c9fa5fc0cfd019b1f878eaf33f2d80223ebb79
arungandhis <144406712+arungandhis@users.noreply.github.com>
2026-04-10 09:54:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+  output: 'export', // Enables static site generation for Render
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
